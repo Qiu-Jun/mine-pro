b0dde6c4be063bcf70c24f4591aefd20acd6654a
June <1601745371@qq.com>
2024-06-04 16:54:01 +0800

    feat: 测试sql

--- packages/server/src/migrations/1717490896815-initData.ts
+++ packages/server/src/migrations/1717490896815-initData.ts
@@ -2,7 +2,7 @@
  * @Author: June
  * @Description:
  * @Date: 2024-06-04 16:48:16
- * @LastEditTime: 2024-06-04 16:49:17
+ * @LastEditTime: 2024-06-04 16:52:47
  * @LastEditors: June
  * @FilePath: \mine-pro\packages\server\src\migrations\1717490896815-initData.ts
  */
@@ -11,7 +11,7 @@ import fs from 'node:fs'
 import path from 'node:path'
 
 const sql = fs.readFileSync(
-  path.join(__dirname, '../../deploy/sql/nest_admin.sql'),
+  path.join(__dirname, '../../deploy/sql/test.sql'),
   'utf8'
 )
 export class InitData1717490896815 implements MigrationInterface {
