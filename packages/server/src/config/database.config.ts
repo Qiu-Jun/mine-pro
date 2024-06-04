fac540399a8e4857d67630549172fbe67f3c69b7
June <1601745371@qq.com>
2024-06-04 17:37:46 +0800

    fix: 🐛 优化配置

--- packages/server/src/config/database.config.ts
+++ packages/server/src/config/database.config.ts
@@ -2,13 +2,13 @@
  * @Author: June
  * @Description:
  * @Date: 2024-06-04 11:32:19
- * @LastEditTime: 2024-06-04 16:01:24
+ * @LastEditTime: 2024-06-04 17:27:23
  * @LastEditors: June
  * @FilePath: \mine-pro\packages\server\src\config\database.config.ts
  */
 import { ConfigType, registerAs } from '@nestjs/config'
 import { DataSource, DataSourceOptions } from 'typeorm'
-import { mysqlConfig, envBoolean } from '@/global/env'
+import { mysqlConfig } from '@/global/env'
 import dotenv from 'dotenv'
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
@@ -19,11 +19,11 @@ const currentScript = process.env.npm_lifecycle_event
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
   host: mysqlConfig.host,
-  port: Number(mysqlConfig.port),
+  port: mysqlConfig.port,
   username: mysqlConfig.username,
   password: mysqlConfig.password,
   database: mysqlConfig.database,
-  synchronize: envBoolean('MYSQL_SYNCHRONIZE', false),
+  synchronize: mysqlConfig.synchronize,
   // 解决通过 pnpm migration:run 初始化数据时，遇到的 SET FOREIGN_KEY_CHECKS = 0; 等语句报错问题, 仅在执行数据迁移操作时设为 true
   multipleStatements: currentScript === 'typeorm',
   entities: ['dist/modules/**/*.entity{.ts,.js}'],
--- packages/server/src/global/env.ts
+++ packages/server/src/global/env.ts
@@ -3,64 +3,152 @@
  */
 export type BaseType = boolean | number | string | undefined | null
 
-/**
- * 格式化环境变量
- * @param key 环境变量的键值
- * @param defaultValue 默认值
- * @param callback 格式化函数
- */
-function fromatValue<T extends BaseType = string>(
-  key: string,
-  defaultValue: T,
-  callback?: (value: string) => T
-): T {
-  const value: string | undefined = process.env[key]
-  if (typeof value === 'undefined') return defaultValue
-
-  if (!callback) return value as unknown as T
-
-  return callback(value)
-}
-
 const {
   SERVER_PORT,
   WS_PORT,
   WS_PATH,
+  APP_NAME,
+  APP_BASE_URL,
+  APP_LOCALE,
+  PROJECT_DIR,
+
+  MULTI_DEVICE_LOGIN,
+
+  LOGGER_LEVEL,
+  LOGGER_MAX_FILES,
+
   MYSQL_HOST,
   MYSQL_PORT,
   MYSQL_DATABASE,
   MYSQL_USERNAME,
   MYSQL_PASSWORD,
   MYSQL_ROOT_PASSWORD,
+  MYSQL_SYNCHRONIZE,
+  MYSQL_LOGGING,
+
   REDIS_HOST,
   REDIS_PORT,
   REDIS_PASSWORD,
-  REDIS_DB
+  REDIS_DB,
+
+  JWT_SECRET,
+  JWT_EXPIRE,
+  REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_EXPIRE,
+
+  SWAGGER_ENABLE,
+  SWAGGER_PATH,
+  SWAGGER_VERSION,
+
+  SMTP_HOST,
+  SMTP_PORT,
+  SMTP_USER,
+  SMTP_PASS
 } = process.env
+
+const envMap = {
+  SERVER_PORT,
+  WS_PORT,
+  WS_PATH,
+  APP_NAME,
+  APP_BASE_URL,
+  APP_LOCALE,
+  PROJECT_DIR,
+
+  MULTI_DEVICE_LOGIN,
+
+  LOGGER_LEVEL,
+  LOGGER_MAX_FILES,
+
+  MYSQL_HOST,
+  MYSQL_PORT,
+  MYSQL_DATABASE,
+  MYSQL_USERNAME,
+  MYSQL_PASSWORD,
+  MYSQL_ROOT_PASSWORD,
+  MYSQL_SYNCHRONIZE,
+  MYSQL_LOGGING,
+
+  REDIS_HOST,
+  REDIS_PORT,
+  REDIS_PASSWORD,
+  REDIS_DB,
+
+  JWT_SECRET,
+  JWT_EXPIRE,
+  REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_EXPIRE,
+
+  SWAGGER_ENABLE,
+  SWAGGER_PATH,
+  SWAGGER_VERSION,
+
+  SMTP_HOST,
+  SMTP_PORT,
+  SMTP_USER,
+  SMTP_PASS
+}
 
 // 服务配置
 export const serverConfig = {
-  port: SERVER_PORT,
-  ws_port: WS_PORT,
-  ws_path: WS_PATH
+  port: envString('SERVER_PORT'),
+  ws_port: envString('WS_PORT'),
+  ws_path: envString('WS_PATH'),
+  app_name: envString('APP_NAME'),
+  app_base_url: envString('APP_BASE_URL'),
+  app_locale: envString('APP_LOCALE'),
+  project_dir: envString('PROJECT_DIR'),
+  multi_device_login: envBoolean('MULTI_DEVICE_LOGIN', true)
+}
+
+// logger
+export const loggerConfig = {
+  logger_lever: envString('LOGGER_LEVEL'),
+  logger_max_files: envNumber('LOGGER_MAX_FILES')
 }
 
 // mysql 配置
 export const mysqlConfig = {
-  host: MYSQL_HOST,
-  port: MYSQL_PORT,
-  database: MYSQL_DATABASE,
-  username: MYSQL_USERNAME,
-  password: MYSQL_PASSWORD,
-  root_password: MYSQL_ROOT_PASSWORD
+  host: envString('MYSQL_HOST'),
+  port: envNumber('MYSQL_PORT', 3306),
+  database: envString('MYSQL_DATABASE'),
+  username: envString('MYSQL_USERNAME'),
+  password: envString('MYSQL_PASSWORD'),
+  synchronize: envBoolean('MYSQL_SYNCHRONIZE', false),
+
+  root_password: envString('MYSQL_ROOT_PASSWORD'),
+  logging: MYSQL_LOGGING
 }
 
 // redis 配置
 export const redisConfig = {
-  host: REDIS_HOST,
-  port: REDIS_PORT,
-  password: REDIS_PASSWORD,
-  db: REDIS_DB
+  host: envString('REDIS_HOST'),
+  port: envNumber('REDIS_PORT', 6379),
+  password: envString('REDIS_PASSWORD'),
+  db: envNumber('REDIS_DB', 0)
+}
+
+// security
+export const jwtConfig = {
+  secret: envString('JWT_SECRET'),
+  expire: envString('JWT_EXPIRE'),
+  refresh_token_expire: envNumber('REFRESH_TOKEN_EXPIRE', 2592000),
+  refresh_token_secret: envString('REFRESH_TOKEN_SECRET')
+}
+
+// swagger
+export const swaggerConfig = {
+  enable: envBoolean('SWAGGER_ENABLE', true),
+  path: envString('SWAGGER_PATH'),
+  version: envString('SWAGGER_VERSION')
+}
+
+// stmp
+export const smtpConfig = {
+  host: envString('SMTP_HOST'),
+  port: envNumber('SMTP_PORT', 465),
+  user: envString('SMTP_USER'),
+  pass: envString('SMTP_PASS')
 }
 
 /**
@@ -71,11 +159,32 @@ export function isDev(): boolean {
   return process.env.NODE_ENV === 'development'
 }
 
-export function envString(key: string, defaultValue: string = '') {
+/**
+ * 格式化环境变量
+ * @param key 环境变量的键值
+ * @param defaultValue 默认值
+ * @param callback 格式化函数
+ */
+
+function fromatValue<T extends BaseType = string>(
+  key: string,
+  defaultValue: T,
+  callback?: (value: string) => T
+): T {
+  const value: string | undefined = envMap[key]
+
+  if (typeof value === 'undefined') return defaultValue
+
+  if (!callback) return value as unknown as T
+
+  return callback(value)
+}
+
+function envString(key: string, defaultValue: string = '') {
   return fromatValue(key, defaultValue)
 }
 
-export function envNumber(key: string, defaultValue: number = 0) {
+function envNumber(key: string, defaultValue: number = 0) {
   return fromatValue(key, defaultValue, (value) => {
     try {
       return Number(value)
@@ -85,7 +194,7 @@ export function envNumber(key: string, defaultValue: number = 0) {
   })
 }
 
-export function envBoolean(key: string, defaultValue: boolean = false) {
+function envBoolean(key: string, defaultValue: boolean = false) {
   return fromatValue(key, defaultValue, (value) => {
     try {
       return Boolean(JSON.parse(value))
@@ -94,3 +203,6 @@ export function envBoolean(key: string, defaultValue: boolean = false) {
     }
   })
 }
+
+console.log('===================================')
+console.log(serverConfig)
